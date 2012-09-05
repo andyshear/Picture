c3575dd9a5d9f8af9780ae01af48099e6d4b816d
Andy <shearar@muohio.edu>
2012-09-05 12:17:48 -0400

    Final commit
    Having problems displaying my picture, tried to utalize your blur method
    but could not get it to work.  My first commit drew a square and a
    triangle, all of which are included in this commit.  I keep getting an
    "Access Violation" error when trying to blur my .jpg

--- src/PictureApp.cpp
+++ src/PictureApp.cpp
@@ -5,6 +5,8 @@
 #include "boost/date_time/posix_time/posix_time.hpp"
 #include "Resources.h"
 
+
+
 using namespace ci;
 using namespace ci::app;
 using namespace std;
@@ -18,10 +20,10 @@ class PictureApp : public AppBasic {
 
 private:
 	Surface* mySurface_;
-	Surface my_pic;
+	
 
-	static const int width = 800;
-	static const int height = 600;
+	static const int width = 604;
+	static const int height = 451;
 	static const int size = 1024;
 
 	uint8_t* my_blur_pattern_;
@@ -29,8 +31,8 @@ private:
 	void drawRectangle (int xPos, int yPos, int width, int height, uint8_t* pixels, Color8u c);
 	void drawCircle (int xPos, int yPos, int radius, uint8_t* pixels, Color8u c);
 	void drawLine (int xPos, int yPos, int xPos2, int yPos2, int length, uint8_t* pixels, Color8u c);
-	void drawTriangle (int xPos, int yPos, int length, uint8_t* pixels, Color8u c);	
-	void blur(uint8_t* image, uint8_t* pattern);
+	void drawTriangle (int xPos, int yPos, int length, uint8_t* pixels, Color8u c);
+	void blur(uint8_t* image_to_blur, uint8_t* blur_pattern);
 	
 };
 
@@ -80,14 +82,78 @@ void PictureApp::drawTriangle (int x, int y, int length, uint8_t* pixels, Color8
 	}
 }
 
-void PictureApp:: blur(uint8_t* image, uint8_t* pattern){
+//References @Bo Brinkman's blur method
+void PictureApp:: blur(uint8_t* image_to_blur, uint8_t* blur_pattern){
 	
+	static uint8_t work_buffer[3*size*size];
+
+	memcpy(work_buffer,image_to_blur,3*size*size);
+
+	uint8_t kernelA[9] =
+	{4,3,4,
+	4,3,4,
+	4,3,4};
+	uint8_t kernelB[9] =
+	{4,3,4,
+	4,2,4,
+	4,3,4};
+
+	uint8_t total_red =0;
+	uint8_t total_green =0;
+	uint8_t total_blue =0;
+	int offset;
+	int k;
+	int y,x,ky,kx;
+
+	for(y=1;y<height-1;y++){
+		for(x=1;y<width-1;x++){
+
+			offset = 3*(x + y*width);
+			if(blur_pattern[offset] < 256/3){
+				total_red =0;
+				total_green =0;
+				total_blue =0;
+				for(ky=-1;ky<=1;ky++){
+					for(kx=-1;kx<=1;kx++){
+						offset = 3*(x + kx + (y+ky)*size);
+						k = kernelA[kx+1 + (ky+1)*3];
+						total_red += (work_buffer[offset] >> k);
+						total_green += (work_buffer[offset+1] >> k);
+						total_blue += (work_buffer[offset+2] >> k);
+					}
+				}
+			}else if(blur_pattern[offset] < 2*256/3){
+				total_red =0;
+				total_green =0;
+				total_blue =0;
+				for(ky=-1;ky<=1;ky++){
+					for(kx=-1;kx<=1;kx++){
+						offset = 3*(x + kx + (y+ky)*size);
+						k = kernelB[kx+1 + (ky+1)*3];
+						total_red += (work_buffer[offset] >> k);
+						total_green += (work_buffer[offset+1] >> k);
+						total_blue += (work_buffer[offset+2] >> k);
+					}
+				}
+			}else {
+				offset = 3*(x + y*size);
+				total_red += work_buffer[offset];
+				total_green += work_buffer[offset+1];
+				total_blue += work_buffer[offset+2];
+			}
+			offset = 3*(x + y*size);
+			image_to_blur[offset] = total_red;
+			image_to_blur[offset+1] = total_green;
+			image_to_blur[offset+2] = total_blue;
+		}
+	}
 }
 
 void PictureApp::setup()
 {
 	mySurface_ = new Surface(size, size, false);
 
+	//References @Bo Brinkman's blur method
 	Surface my_pic(loadImage( loadResource(RES_PICTURE) ));
 	uint8_t* blur_data = my_pic.getData();
 	my_blur_pattern_ = new uint8_t[width*height*3];
